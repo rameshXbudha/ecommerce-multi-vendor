4bd4aaa95902484a21d1543ffaf5b20cdd80aa23
Ramesh Budha <rameshxbudha@gmail.com>
2025-06-04 08:15:37 +0545

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Hero from "./component/Hero";
+import About from "./component/About";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/header' element={<Header />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/hero' element={<Hero />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
